0ba4c427b8b9c99ecf9835523cda524d0c8800ec
jamaal0987 <Jamalmw22@gmail.com>
2026-02-06 21:54:11 +0300

    Enhance temperature conversion with history tracking
    Added temperature conversion history and validation checks.

--- assignment.js
+++ assignment.js
@@ -1,29 +1,70 @@
-// USING DOM
+
+// USING AN ARRAY
+let conversionHistory = []; 
+
 const btn = document.getElementById('convertBtn');
 const res = document.getElementById('resultText');
 
-// USING EVENTS
 btn.addEventListener('click', function() {
     const temp = document.getElementById('inputTemp').value;
     const unit = document.getElementById('unitType').value;
 
-// INPUT VALIDATING
-    if (temp === "") {
+// 2 HAGAANI VALIDATION
+    if (temp === "" || isNaN(temp)) {
         res.style.color = "red";
-        res.innerHTML = "ops you need to enter a number!";
+        res.innerHTML = "Ops you need to enter a valid number!";
         return;
     }
 
     res.style.color = "white";
     let val = parseFloat(temp);
-    
-// CALCULATING THE LOGIC
+
+    // USING LOGICALL
+    if (unit === "celcius to farenhite" && val < -273.15) {
+        res.innerHTML = "Error: Temperature below -273.15°C is impossible!";
+        return;
+    }
+// in order si an ugu shabano natijada soo abxda hadhowti
+    let finalResult = "";
+
     if (unit === "celcius to farenhite") {
         let f = (val * 9/5) + 32;
-        res.innerHTML = val + "°C = " + f.toFixed(2) + "°F";
+        finalResult = val + "°C = " + f.toFixed(2) + "°F";
     } else {
         let c = (val - 32) * 5/9;
-        res.innerHTML = val + "°F = " + c.toFixed(2) + "°C";
+        finalResult = val + "°F = " + c.toFixed(2) + "°C";
     }
 
+    res.innerHTML = finalResult;
+
+    // 4. using objects again
+    let dataObject = {
+        inputValue: val,
+        unitType: unit,
+        resultValue: finalResult,
+        date: new Date().toLocaleTimeString()
+    };
+    conversionHistory.push(dataObject);
+    console.log("History Updated:", conversionHistory);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
